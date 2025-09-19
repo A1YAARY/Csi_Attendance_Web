f674088a0aecdad08aaeaf7b40cc2fd32e813c5a
tikmaram prajapati <tikmaramprajapati10b@gmail.com>
2025-09-19 22:42:11 +0530

    imported . env

--- client/src/context/Admin_context.jsx
+++ client/src/context/Admin_context.jsx
@@ -5,4 +5,6 @@ const AdminContext = createContext();
 export function AdminProvider({ children }) {
   const [activeAdminView, setActiveAdminView] = useState("home");
 
-    const baseurl = "https://csi-attendance-web.onrender.com";}  
+  // const baseurl = "https://csi-attendance-web.onrender.com";
+  const baseurl = import.meta.env.VITE_BACKEND_BASE_URL;
+}  
